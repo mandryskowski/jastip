bb903f585c95d1e88a47fc1b1186ce9601c053f8
mandryskowski <nedesf@gmail.com>
2024-05-28 21:59:13 +0100

    Changed integration test

--- frontend/jastip/test_driver/app_test.dart
+++ frontend/jastip/test_driver/app_test.dart
@@ -1,25 +1,29 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_driver/flutter_driver.dart' as Fd;
+import 'package:jastip/main.dart';
 
 void main() {
-  group('Jastip', () {
-    late Fd.FlutterDriver driver;
+  // ···
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter',
+        (tester) async {
+      // Load app widget.
+      await tester.pumpWidget(const MyApp());
 
-    setUpAll(() async {
-      driver = await Fd.FlutterDriver.connect();
+      // Verify the counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+
+      // Finds the floating action button to tap on.
+      final fab = find.byKey(const ValueKey('increment'));
+
+      // Emulate a tap on the floating action button.
+      await tester.tap(fab);
+
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+
+      // Verify the counter increments by 1.
+      expect(find.text('1'), findsOneWidget);
     });
-
-    tearDownAll(() async {
-      if (driver != null) {
-        driver.close();
-      }
-    });
-
-    test('Check if app is running', () async {
-      // Check if a specific widget is present on the screen
-      expect(await driver.getText(Fd.find.text('Welcome')), 'Welcome'); // Fully qualify find method
-    });
-
-    // Add more integration tests as needed
   });
 }
