15321893c231e7450f56b93511c76f6c431d354a
mandryskowski <nedesf@gmail.com>
2024-05-28 17:23:07 +0100

    Fixed test driver

--- frontend/jastip/test_driver/app.dart
+++ frontend/jastip/test_driver/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_driver/.dart';
+import 'package:flutter_driver/flutter_driver.dart';
 
 void main() {
   group('MyApp', () {
