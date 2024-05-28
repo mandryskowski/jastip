189bce49971012dd0b8a48a76a9c81a5087d7704
mandryskowski <nedesf@gmail.com>
2024-05-28 17:03:53 +0100

    Added flutter driver

---
+++ frontend/jastip/test_driver/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_driver/.dart';
+
+void main() {
+  group('MyApp', () {
+    FlutterDriver driver;
+
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+
+    test('Check if app is running', () async {
+      // Check if a specific widget is present on the screen
+      expect(await driver.getText(find.text('Welcome')), 'Welcome');
+    });
+
+    // Add more integration tests as needed
+  });
+}
