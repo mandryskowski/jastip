5e91fe4256944a46e380f54ecfdeaed37e110fb6
Jakub Lapinski <jakub.lapinski22@imperial.ac.uk>
2024-06-02 09:07:20 +0100

    Updated visual apperaence of the menu

--- frontend/jastip/lib/menu.dart
+++ frontend/jastip/lib/menu.dart
@@ -3,6 +3,8 @@ import 'package:jastip/Constants.dart';
 import 'DeliveryCarrierPage.dart';
 
 class MenuBox extends StatelessWidget {
+  const MenuBox({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -14,14 +16,17 @@ class MenuBox extends StatelessWidget {
             height: 130.0,
             child: DrawerHeader(
               decoration: BoxDecoration(
-                color: Colors.white,
+                color: backgroundColorData, // Subtle background color
               ),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundImage: NetworkImage('https://cdn.frankerfacez.com/emoticon/660211/4'),
+                  Padding(
+                    padding: EdgeInsets.all(8.0), // Padding around the avatar
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage('https://cdn.frankerfacez.com/emoticon/660211/4'),
+                    ),
                   ),
                   SizedBox(width: 10),
                   Column(
@@ -53,22 +58,25 @@ class MenuBox extends StatelessWidget {
               ),
             ),
           ),
-          SectionTitle(title: 'Transport and Delivery'),
+          const Padding(
+            padding: EdgeInsets.only(top: 8.0), // Space above section title
+            child: SectionTitle(title: 'Transport and Delivery'),
+          ),
           ListTile(
-            leading: Icon(Icons.assignment),
-            title: Text('My Orders'),
+            leading: const Icon(Icons.assignment),
+            title: const Text('My Orders'),
             onTap: () {},
           ),
           ListTile(
-            leading: Icon(Icons.assignment_return),
-            title: Text('My Drop-Offs'),
+            leading: const Icon(Icons.assignment_return),
+            title: const Text('My Drop-Offs'),
             onTap: () {},
           ),
-          Divider(),
-          SectionTitle(title: 'Services'),
+          const PaddedDivider(),
+          const SectionTitle(title: 'Services'),
           ListTile(
-            leading: Icon(Icons.send),
-            title: Text('Send Package'),
+            leading: const Icon(Icons.send),
+            title: const Text('Send Package'),
             onTap: () {
               Navigator.push(
                 context,
@@ -77,8 +85,8 @@ class MenuBox extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: Icon(Icons.directions_car),
-            title: Text('Transport Package'),
+            leading: const Icon(Icons.directions_car),
+            title: const Text('Transport Package'),
             onTap: () {
               Navigator.push(
                 context,
@@ -86,29 +94,29 @@ class MenuBox extends StatelessWidget {
               );
             },
           ),
-          Divider(),
-          SectionTitle(title: 'Support'),
+          const PaddedDivider(),
+          const SectionTitle(title: 'Support'),
           ListTile(
-            leading: Icon(Icons.support_agent),
-            title: Text('Customer Support'),
+            leading: const Icon(Icons.support_agent),
+            title: const Text('Customer Support'),
             onTap: () {},
           ),
-          Divider(),
-          SectionTitle(title: 'Account'),
+          const PaddedDivider(),
+          const SectionTitle(title: 'Account'),
           ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('Account Details'),
+            leading: const Icon(Icons.account_circle),
+            title: const Text('Account Details'),
             onTap: () {},
           ),
           ListTile(
-            leading: Icon(Icons.logout),
-            title: Text('Log Out'),
+            leading: const Icon(Icons.logout),
+            title: const Text('Log Out'),
             onTap: () {},
           ),
-          Divider(),
+          const PaddedDivider(),
           ListTile(
-            leading: Icon(Icons.info),
-            title: Text('About Us'),
+            leading: const Icon(Icons.info),
+            title: const Text('About Us'),
             onTap: () {},
           ),
         ],
@@ -118,26 +126,26 @@ class MenuBox extends StatelessWidget {
 
   PageRouteBuilder coolHomeTransition(int initialIndex) {
     return PageRouteBuilder(
-            pageBuilder: (context, animation, secondaryAnimation) => HomePage(initialIndex: initialIndex),
-            transitionsBuilder: (context, animation, secondaryAnimation, child) {
-              var begin = Offset(1.0, 0.0);
-              var end = Offset.zero;
-              var curve = Curves.ease;
+      pageBuilder: (context, animation, secondaryAnimation) => HomePage(initialIndex: initialIndex),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = const Offset(1.0, 0.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
 
-              var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
-              var offsetAnimation = animation.drive(tween);
+        var offsetAnimation = animation.drive(tween);
 
-              return SlideTransition(
-                position: offsetAnimation,
-                child: child,
-              );
-            },
-          );
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
+      },
+    );
   }
 }
 
- class SectionTitle extends StatelessWidget {
+class SectionTitle extends StatelessWidget {
   final String title;
 
   const SectionTitle({
@@ -148,18 +156,26 @@ class MenuBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              title,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          );
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0), // Add vertical padding
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
   }
 }
 
+class PaddedDivider extends StatelessWidget {
+  const PaddedDivider({super.key});
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0), // Space around dividers
+            child: Divider(),
+          );
+  }
+}
